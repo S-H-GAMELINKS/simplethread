52c86e30f0b8778cf02c68741f529f0700d28149
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-29 17:04:53 +0900

    add create topics form

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,3 +1,10 @@
 @foreach($topics as $topic)
     <a href="/topics/{{ $topic->id}}">{{ $post->title }}</a>
 @endforeach
+
+<form method="POST" action="/topics">
+    {{ csrf_field() }}
+    <input type="text" name="title">
+    <input type="text" name="content">
+    <input type="submit">
+</form>
